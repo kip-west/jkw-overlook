2441a05f04c2f602784dd919b69d1fcde54c7258
Jake West <jacobkwest93@gmail.com>
2020-07-30 15:53:34 -0600

    Write test & implementation code for User.js properties #14

--- test/User-test.js
+++ test/User-test.js
@@ -3,7 +3,27 @@ import { expect } from 'chai';
 import User from '../src/User'
 
 describe.only('User', function() {
+  let user1, user2;
+
+  beforeEach(function() {
+    user1 = new User({
+      id: 1,
+      name: 'Belinda Jenkins'
+    })
+  })
   it('should be a function', function() {
     expect(User).to.be.a('function');
+  })
+
+  it('should be an instance of User', function() {
+    expect(user1).to.be.an.instanceof(User)
+  })
+
+  it('should have an id', function() {
+    expect(user1.id).to.equal(1)
+  })
+
+  it('should have a name', function() {
+    expect(user1.name).to.equal('Belinda Jenkins')
   })
 })
