6e8c8eb8379b1e93366059c5105ae02ff012bbef
Jake West <jacobkwest93@gmail.com>
2020-07-30 14:46:54 -0600

    Add hidden styling & dummy info for customer/manager dashboards

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
+import domUpdates from '../src/domUpdates';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const loginSubmitButton = document.getElementById("submit-login");
+
+loginSubmitButton.addEventListener("click", checkLoginSubmission);
+
+function checkLoginSubmission() {
+  event.preventDefault();
+  console.log(document.querySelector('.manager-dashboard'));
+}
