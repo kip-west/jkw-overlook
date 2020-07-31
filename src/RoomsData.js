b58e6d146e08e88dbad8cdecd4cbef5f37a54181
Jake West <jacobkwest93@gmail.com>
2020-07-31 14:43:56 -0600

    Add findRoomByType test code & implementation code #11

--- src/RoomsData.js
+++ src/RoomsData.js
@@ -2,6 +2,10 @@ class RoomsData {
   constructor(roomsData) {
     this.rooms = roomsData.rooms;
   }
+
+  findRoomByType(roomType) {
+    return this.rooms.filter(room => room.roomType === roomType);
+  }
 }
 
 export default RoomsData
