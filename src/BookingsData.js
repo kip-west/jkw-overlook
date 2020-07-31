29135383fb30e29f9b50c9fcc65abf552f39f1a2
Jake West <jacobkwest93@gmail.com>
2020-07-31 15:38:16 -0600

    Add implementation code for findBookingByDate #10

--- src/BookingsData.js
+++ src/BookingsData.js
@@ -2,6 +2,10 @@ class BookingsData {
   constructor(bookingData) {
     this.bookings = bookingData.bookings;
   }
+
+  findBookingByDate(date) {
+    return this.bookings.filter(booking => booking.date === date)
+  }
 }
 
 export default BookingsData
