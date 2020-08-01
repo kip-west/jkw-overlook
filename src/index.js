2492019c3ead1584cf0e92f04216b2e43bf9c65c
Jake West <jacobkwest93@gmail.com>
2020-08-01 15:16:23 -0600

    Add getDataFromServer & create hotelData object #32

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import domUpdates from '../src/domUpdates';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png';
 
+let hotelData = {
+  usersData: null,
+  roomsData: null,
+  bookingsData: null
+}
+
+window.onload = getDataFromServer();
+
 const loginSubmitButton = document.getElementById("submit-login");
 loginSubmitButton.addEventListener("click", checkLoginSubmission);
 
@@ -16,4 +24,24 @@ function checkLoginSubmission() {
     domUpdates.hideAll();
     domUpdates.checkUsername();
   }
+}
+
+function createHotelData(usersData, roomsData, bookingsData) {
+  hotelData.usersData = usersData;
+  hotelData.roomsData = roomsData;
+  hotelData.bookingsData = bookingsData;
+
+  return hotelData;
+}
+
+/*----------GET/POST/DELETE Functions----------*/
+function getDataFromServer() {
+  Promise.all([
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users'),
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms'),
+    fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  ])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(([usersData, roomsData, bookingsData]) => createHotelData(usersData, roomsData, bookingsData))
+    .catch(err => console.error(err))
 }
