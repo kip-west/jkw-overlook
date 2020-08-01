87d77b8a7c58f4cb9ef389618bf6258ccaf0aa08
Jake West <jacobkwest93@gmail.com>
2020-07-31 19:39:49 -0600

    Add implementation code for findUserByName(name) #24

--- src/CustomerData.js
+++ src/CustomerData.js
@@ -2,6 +2,12 @@ class CustomerData {
   constructor(userData) {
     this.users = userData.users;
   }
+
+  findUserByName(name) {
+    let foundUser = this.users.find(user => user.name === name);
+
+    return foundUser;
+  }
 }
 
 export default CustomerData
